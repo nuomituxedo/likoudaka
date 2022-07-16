efe1d135fc420a9f858b9044620cdfd17c185cd9
nuomituxedo <67388217+nuomituxedo@users.noreply.github.com>
2022-07-15 23:14:27 -0700

    Time: 88 ms (45.83%), Space: 13.9 MB (54.90%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -1,20 +1,19 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        chars = list(s)
         res = []
         last_c = 0
-        for i in range(0, len(chars)):
-            res.append(len(chars))
-        for i in range(0, len(chars)):
-            if chars[i] == c:
+        for i in range(0, len(s)):
+            res.append(len(s))
+        for i in range(0, len(s)):
+            if s[i] == c:
                 #update left hand side up to the last seen c
                 for j in range (last_c, i):
                     res[j] = min(res[j], i-j)
                 last_c = i
                 #update right hand side
-                for j in range (i, len(chars)):
+                for j in range (i, len(s)):
                     res[j] = j - i
                     # stop updating right side if see next c
-                    if chars[j] == c:
+                    if s[j] == c:
                         continue
         return res
