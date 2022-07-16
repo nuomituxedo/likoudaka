a24dd9b5d1ffc9cf32fd92528e1bc11ad4025112
nuomituxedo <67388217+nuomituxedo@users.noreply.github.com>
2022-07-15 22:44:24 -0700

    Time: 94 ms (40.93%), Space: 13.9 MB (91.29%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -2,12 +2,14 @@ class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         chars = list(s)
         res = []
+        last_c = 0
         for i in range(0, len(chars)):
             res.append(len(chars))
         for i in range(0, len(chars)):
             if chars[i] == c:
-                for j in range (0, i):
+                for j in range (last_c, i):
                     res[j] = min(res[j], i-j)
+                last_c = i
                 for j in range (i, len(chars)):
                     res[j] = j - i
         return res
