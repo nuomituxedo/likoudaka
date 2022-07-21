bf0b75852b10a098f1807a4b15eba9eb26db555c
nuomituxedo <67388217+nuomituxedo@users.noreply.github.com>
2022-07-20 19:29:24 -0700

    Time: 54 ms (59.57%), Space: 13.9 MB (30.40%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -5,34 +5,39 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        #return if head is empty
         if not head: return None
+        
+        #find out the total number of nodes in the linkedlist
+        #apply modulo to k to get the actual number of rotations needed
         length = -1
         dummy = ListNode(0)
         dummy.next = head
-        print(dummy)
         while dummy:
             length += 1
             dummy = dummy.next
-            print(dummy)
         k = k % length
+        #if no rotation is needed, directly return head
         if k == 0: return head
-        print(k)
+        
+        # 1. create a dummy node, travel the list nodes to the node to be cut (new end)
+        # 2. after this operation we end up with 2 smaller linkedlists
         dummy = ListNode(0)
         dummy.next = head
-        print(dummy)
         for i in range(0, length - k):
             dummy = dummy.next
-            print(dummy)
         new_head = ListNode(0)
-        new_head.next = dummy.next
-        print(new_head)
-        dummy.next = None
+        new_head = dummy.next # new_head points to the new head node
         
+        dummy.next = None #make dummy the end node, now we have 2 linkedlists
+        
+        #3. create dummy2 to point to the second linkedlist and travel to its end node
         dummy2 = ListNode(0)
-        dummy2 = new_head.next
-        print('000',dummy2)
+        dummy2 = new_head
+        
         while dummy2.next:
             dummy2 = dummy2.next
-        print('ppp', dummy2)
+        
+        #4. now relink the two linkedlists
         dummy2.next = head
-        return new_head.next
+        return new_head
