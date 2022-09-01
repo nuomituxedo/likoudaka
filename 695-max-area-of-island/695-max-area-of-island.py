c97295583580619e292b58ab4a5cc0fb54a6df0c
nuomituxedo <67388217+nuomituxedo@users.noreply.github.com>
2022-09-01 16:28:29 -0700

    Time: 234 ms (40.93%), Space: 16.5 MB (59.41%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,22 @@
+class Solution:
+    #DFS
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        maxArea = 0
+        m = len(grid)
+        n = len(grid[0])
+        
+        def dfs(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == 0:
+                return 0
+            
+            grid[x][y] = 0 #mark visited
+            
+            return 1 + dfs(x-1,y) + dfs(x+1,y) + dfs(x,y-1) + dfs(x,y+1)
+        
+        for i in range(0, m):
+            for j in range(0, n):
+                if grid[i][j] == 1:
+                    maxArea = max(maxArea, dfs(i, j))
+        return maxArea
+                    
+        
